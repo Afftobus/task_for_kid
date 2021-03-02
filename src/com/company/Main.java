4579b76f0b7f19bea78b4fb8db787ab016779359
Artem Filippoff <ar.filippov@hh.ru>
2021-03-02 20:16:41 +0300

    task 003

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
 public class Main {
+    /* Напиши функцию, которая принимает массив чисел и ищет максимальное число
+        числв в массиве могут быть только положительные
+     */
+
+    static maximalnoe
 
     public static void main(String[] args) {
-	// write your code here
+        int[] myArray;
+
+        myArray = new int[]{5, 6, 7, 2, 22, 31, 16};
+        if (maximalnoe(myArray) == 31) System.out.println("Good job!");
+        else System.out.println("ERROR!");
+
+        myArray = new int[]{0};
+        if (maximalnoe(myArray) == 0) System.out.println("Good job!");
+        else System.out.println("ERROR!");
+
+        myArray = new int[]{-2, -3};
+        if (maximalnoe(myArray) == 0) System.out.println("Good job!");
+        else System.out.println("ERROR!");
+
+        myArray = new int[]{};
+        if (maximalnoe(myArray) == 0) System.out.println("Good job!");
+        else System.out.println("ERROR!");
+
     }
 }
