07d537ff140679c6774a73a0672ed36d9dcd6b7b
Artem Filippoff <ar.filippov@hh.ru>
2021-03-02 08:35:40 +0300

    task 001

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
 public class Main {
+  /* Напиши тут функцию kvadrat, которая принимает один аргумент типа int,
+  то есть челое число  и возвращает квадрвт этого числа. Также в типе int.
+
+  */
+    static kvadrat
 
     public static void main(String[] args) {
-	// write your code here
+	      if (kvadrat(2) == 4){
+	          System.out.println("Good Job!");
+        }
+	      if (kvadrat(0) == 0){
+	          System.out.println("Good Job!");
+        }
+	      if (kvadrat(3) == 9){
+	          System.out.println("Good Job!");
+        }
+	      if (kvadrat(-3) == 9){
+	          System.out.println("Good Job!");
+        }
     }
 }
