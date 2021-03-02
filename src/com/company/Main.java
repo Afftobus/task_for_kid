62f9311892f5826a64c700173f9c809c11928ac7
Artem Filippoff <ar.filippov@hh.ru>
2021-03-02 08:38:43 +0300

    task 002

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
 public class Main {
+    /* Напиши функцию processNumbers которая принимает 2 аргумента (a и b) и
+     * если a > b возвращает их сумму, иначе разность b - a */
+    static processNumbers
 
     public static void main(String[] args) {
-	// write your code here
+        if (processNumbers(2,3) == 1){
+            System.out.println("Good job!");
+        }
+        if (processNumbers(4,3) == 7){
+            System.out.println("Good job!");
+        }
+        if (processNumbers(0,0) == 0){
+            System.out.println("Good job!");
+        }
+        if (processNumbers(-5,-3) == 2){
+            System.out.println("Good job!");
+        }
     }
 }
